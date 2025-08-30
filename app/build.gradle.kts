885644d02db65e0913bc1e091722bcf55706aaec
Zanaty <mohamed.zanaty8805@gmail.com>
2025-08-30 04:21:18 +0300

    add resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,13 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.material.v190)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 }
